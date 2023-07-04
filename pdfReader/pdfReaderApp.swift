fe6b11def917a0443322265ba6098ba5c89dba06
Hoa Pham <hoa.phamlethai@gmail.com>
2023-07-04 09:27:10 -0700

    Initial Commit

---
+++ pdfReader/pdfReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdfReaderApp.swift
+//  pdfReader
+//
+//  Created by Hoa Pham on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct pdfReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
